e85133f19231f88a8eeb300117ebe2800c5c96f7
hogedamari <29653357+foo2810@ users.noreply.github.com>
2026-05-19 22:02:51 +0900

    Refactor logging code

--- lib/utils.py
+++ lib/utils.py
@@ -1,49 +1,80 @@
 import sys
+import enum
 
-SYSTEMD_LOGGING = False
+
+def _log_primitive(stream, text: str):
+    stream.write(text)
+    stream.flush()
+
+
+def _log_with_prefix(stream, prefix: str, msg: str):
+    # In multi-line messages, the second and subsequent lines do not start
+    # with the log-level prefix, so they are not recorded
+    # under an unintended log level.
+    #
+    # Implementation note:
+    # If msg is empty, msg[-1] will raise an IndexError. So use msg[-1:] instead.
+    text = msg[:-1].replace("\n", f"\n{prefix}") + msg[-1:]
+    text = f"{prefix}{text}"
+
+    _log_primitive(stream, text)
+
+
+class _LogLevel(enum.IntEnum):
+    ERROR = 0
+    WARN = 1
+    INFO = 2
+    DEBUG = 3
+
+
+class _LoggingType(enum.StrEnum):
+    NORMAL = "normal-logging"
+    SYSTEMD = "systemd-logging"
+
+
+LOGGING_TYPE = _LoggingType.NORMAL
+
+
+PREFIX_TABLE = {
+    _LoggingType.NORMAL: {
+        _LogLevel.ERROR: "Error: ",
+        _LogLevel.WARN: "Warn: ",
+        _LogLevel.INFO: "Info: ",
+        _LogLevel.DEBUG: "Debug: ",
+    },
+    _LoggingType.SYSTEMD: {
+        _LogLevel.ERROR: "<3>",
+        _LogLevel.WARN: "<4>",
+        _LogLevel.INFO: "<6>",
+        _LogLevel.DEBUG: "<7>",
+    },
+}
+
+
 def enable_systemd_logging():
-    global SYSTEMD_LOGGING
-    SYSTEMD_LOGGING = True
+    global LOGGING_TYPE
+    LOGGING_TYPE = _LoggingType.SYSTEMD
+
 
 def info(msg, end="\n"):
-    global SYSTEMD_LOGGING
-    
-    prefix = "Info: "
-    if SYSTEMD_LOGGING:
-        prefix = "<6>"
-
-    print(f"{prefix}{msg}", end=end)
-    sys.stdout.flush()
+    global LOGGING_TYPE
+    prefix = PREFIX_TABLE[LOGGING_TYPE][_LogLevel.INFO]
+    _log_with_prefix(sys.stdout, prefix, f"{msg}{end}")
 
 
 def debug(msg, end="\n"):
-    global SYSTEMD_LOGGING
-
-    prefix = "Debug: "
-    if SYSTEMD_LOGGING:
-        prefix = "<7>"
-
-    print(f"{prefix}{msg}", end=end)
-    sys.stdout.flush()
+    global LOGGING_TYPE
+    prefix = PREFIX_TABLE[LOGGING_TYPE][_LogLevel.DEBUG]
+    _log_with_prefix(sys.stdout, prefix, f"{msg}{end}")
 
 
 def warn(msg, end="\n"):
-    global SYSTEMD_LOGGING
-
-    prefix = "Warn: "
-    if SYSTEMD_LOGGING:
-        prefix = "<4>"
-
-    print(f"{prefix}{msg}", end=end)
-    sys.stdout.flush()
+    global LOGGING_TYPE
+    prefix = PREFIX_TABLE[LOGGING_TYPE][_LogLevel.WARN]
+    _log_with_prefix(sys.stdout, prefix, f"{msg}{end}")
 
 
 def error(msg, end="\n"):
-    global SYSTEMD_LOGGING
-
-    prefix = "Error: "
-    if SYSTEMD_LOGGING:
-        prefix = "<3>"
-
-    print(f"{prefix}{msg}", end=end, file=sys.stderr)
-    sys.stderr.flush()
+    global LOGGING_TYPE
+    prefix = PREFIX_TABLE[LOGGING_TYPE][_LogLevel.ERROR]
+    _log_with_prefix(sys.stderr, prefix, f"{msg}{end}")
